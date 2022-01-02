857021d7de02975b31259c1563dc6b988d3bbfd8
unknown <derek.slauson5@gmail.com>
2022-01-02 14:35:51 -0600

    Hook isn't quite working but it at least doesn't delete all reviews anymore

--- models/review.js
+++ models/review.js
@@ -4,7 +4,11 @@ let reviewSchema = new mongoose.Schema({
     author: {type: String, default: "Anonymous"},
     worthListeningTo: {type: Boolean, default: false},
     stars: {type: Number, required: true},
-    content: {type: String, default: ""}
+    content: {type: String, default: ""},
+    album: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Album"
+    }
 })
 
 module.exports = mongoose.model("Review", reviewSchema)
