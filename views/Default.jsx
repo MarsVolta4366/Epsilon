2d6c824df9158ffba4987359349868b8edac454c
unknown <derek.slauson5@gmail.com>
2022-01-02 15:03:56 -0600

    Changed title

--- views/Default.jsx
+++ views/Default.jsx
@@ -7,7 +7,7 @@ function def(html) {
             <meta charSet="UTF-8" />
             <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>Epsilon</title>
+            <title>AlbumCRUD</title>
             {/* CSS */}
             <link rel="stylesheet" href="/css/styles.css" />
             {/* Font Awesome */}
