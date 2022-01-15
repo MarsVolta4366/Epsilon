ca0c4233fc4c47e69c800fff865d53b376ffd572
unknown <derek.slauson5@gmail.com>
2022-01-15 14:58:50 -0600

    Uncommented views middleware

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const methodOverride = require("method-override")
 const app = express()
 
 // MIDDLEWARE
-// app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views')
 app.set("view engine", "jsx")
 app.engine("jsx", require("express-react-views").createEngine())
 // Below line is needed to get info from req.body
